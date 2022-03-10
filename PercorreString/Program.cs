624cfacb130b73994c14c59af524e257438dc373
Terrorspy <diogocaetanosoares922@gmail.com>
2022-03-10 14:52:17 +0000

    Escreve um frase e pede um Carácter para tirar esse carácter

---
+++ PercorreString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PercorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.WriteLine("String: ");
+         string text = Console.ReadLine();
+
+         Console.Write("Carácter: ");
+         string userinput = Console.ReadLine();
+         string newText = text.Replace(userinput, "");
+
+         Console.WriteLine(newText);
+         Console.ReadLine();
+        }
+    }
+}
